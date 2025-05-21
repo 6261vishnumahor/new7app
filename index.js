2aadd21c98e621fc312a8e9a68ec77bb4f814a50
6261vishnumho <7225vishnumahor@gmail.com>
2025-05-21 15:51:05 +0530

    add files

--- index.js
+++ index.js
@@ -9,12 +9,7 @@ const router=require("./route/router.js")
 app.set("view engine","ejs")
 app.use(express.static("public"))
 app.use(router)
-// app.get("/",(req,res)=>{
-//     res.send("this is a home page")
-// })
-// app.get("/about",(req,res)=>{
-//     res.send("this is about page")
-// })
+
 app.listen(port,()=>{
     console.log("the port is running is 4000")
 })
